178fbbf2eeac11915b85c971e735d97018bcb449
David Cuthell <dcc1447@gmail.com>
2018-11-23 01:59:33 -0500

    Updates

---
+++ precache-manifest.0f6f68d875ff5ebed6083f76e0c0f7ef.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "0dc85b848a62b9856b9a",
+    "url": "/static/css/main.d6c5b5bc.chunk.css"
+  },
+  {
+    "revision": "a100c1766d58417a30ba497d5bfe306a",
+    "url": "/static/media/albumback.a100c176.png"
+  },
+  {
+    "revision": "4612891af998df1f7bed",
+    "url": "/static/js/1.4612891a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "47bbd5672ceacebbe15f6fd4e3b0b4bd",
+    "url": "/static/media/reallogo.47bbd567.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "0dc85b848a62b9856b9a",
+    "url": "/static/js/main.0dc85b84.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a56e54efafedc1a5f0c293af45f0a93c",
+    "url": "/static/media/boat.a56e54ef.jpg"
+  },
+  {
+    "revision": "3b428ee596fe5f4d50877b558cc4bfa2",
+    "url": "/static/media/albumfront.3b428ee5.png"
+  },
+  {
+    "revision": "4612891af998df1f7bed",
+    "url": "/static/css/1.29af0ee4.chunk.css"
+  },
+  {
+    "revision": "9f59b6d653437c527d458a15e75fdcaf",
+    "url": "/index.html"
+  }
+];
